d397625a4ab5694bb9c76df3dab5016157c12b8b
RADISSON lionel <lionel.radisson@gmail.com>
2018-02-06 12:03:55 +0100

    fix char offset

--- code_trinket/communication_functions.ino
+++ code_trinket/communication_functions.ino
@@ -19,31 +19,41 @@ void parseString() {
         sendStates();
     }
     else if( inputString.startsWith( "setPump:" ) ) {
-        char state = inputString.charAt( inputStringLength );
+        char state = inputString.charAt( inputStringLength - 1 );
         if( state == '0' ){
             setPump( LOW );
+            Serial.println( "OK: PUMP OFF" );
         }
         else if( state == '1' ){
             setPump( HIGH );
+            Serial.println( "OK: PUMP ON" );
         }
     }
     else if( inputString.startsWith( "setWarmLight:" ) ) {
-        char state = inputString.charAt( inputStringLength );
+        char state = inputString.charAt( inputStringLength - 1 );
         if( state == '0' ){
             setLeds( WARM_LED_PIN, LOW );
+            Serial.println( "OK: WARM LEDS OFF" );
         }
         else if( state == '1' ){
             setLeds( WARM_LED_PIN, HIGH );
+            Serial.println( "OK: WARM LEDS ON" );
         }
     }
     else if( inputString.startsWith( "setColdLight:" ) ) {
-        char state = inputString.charAt( inputStringLength );
+        char state = inputString.charAt( inputStringLength - 1 );
         if( state == '0' ){
             setLeds( COLD_LED_PIN, LOW );
+            Serial.println( "OK: COLD LEDS OFF" );
         }
         else if( state == '1' ){
             setLeds( COLD_LED_PIN, HIGH );
+            Serial.println( "OK: COLD LEDS ON" );
         }
+    }
+    else {
+        Serial.print( "CMD ERROR:" );
+        Serial.println( inputString );
     }
 
     // reset inputString
@@ -55,7 +65,7 @@ void sendStates(){
     Serial.println( "waterLvl:" + String( waterLvl ) );
     Serial.println( "temp:" + String( temp ) );
     Serial.println( "light:" + String( light ) );
-    Serial.println( "coldLightIntensity" + String( coldLightIntensity ) );
-    Serial.println( "warmLightIntensity" + String( warmLightIntensity ) );
-    Serial.println( "pumpIntensity" + String( pumpIntensity ) );
+    Serial.println( "coldLight:" + String( coldLightIntensity ) );
+    Serial.println( "warmLight:" + String( warmLightIntensity ) );
+    Serial.println( "pump:" + String( pumpIntensity ) );
 }
